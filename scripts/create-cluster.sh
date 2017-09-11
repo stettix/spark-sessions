6750cf45283ac74f4831453285033d6ec592aaa3
Jan Stette <jan.stette@gmail.com>
2017-09-11 18:50:43 +0100

    Tweak cluster configs

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -6,10 +6,10 @@ KEYNAME=${1? "Must provide KeyName parameter"}
 
 CLUSTERID=$(aws emr create-cluster --name "Spark sessionization" --release-label emr-5.8.0 \
   --tags "Name=Spark Sessionization" \
-  --applications Name=Spark Name=Ganglia \
-  --instance-type m4.xlarge \
-  --instance-count 4 \
+  --applications Name=Spark Name=Hadoop Name=Ganglia \
+  --instance-groups '[{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"m4.4xlarge","Name":"Driver","BidPrice":"0.20"},{"InstanceCount":4,"InstanceGroupType":"CORE","InstanceType":"m4.4xlarge","Name":"Worker nodes","BidPrice":"0.20"}]' \
   --log-uri 's3://logs.multe.co.uk/elasticmapreduce/' \
   --enable-debugging \
   --use-default-roles \
+  --configurations file://./spark-configs.json \
   --ec2-attributes KeyName=$KEYNAME,SubnetId=subnet-c434a8a3,AdditionalMasterSecurityGroups=sg-49a3e531| grep -o 'j-\w*')
