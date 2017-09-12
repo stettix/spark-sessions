975707b42a0a80ca3b08a95f2332e4c39f8927bd
Jan Stette <jan.stette@gmail.com>
2017-09-12 12:49:59 +0100

    Increase EBS volume size on cluster nodes

--- scripts/create-cluster.sh
+++ scripts/create-cluster.sh
@@ -7,7 +7,45 @@ KEYNAME=${1? "Must provide KeyName parameter"}
 CLUSTERID=$(aws emr create-cluster --name "Spark sessionization" --release-label emr-5.8.0 \
   --tags "Name=Spark Sessionization" \
   --applications Name=Spark Name=Hadoop Name=Ganglia \
-  --instance-groups '[{"InstanceCount":1,"InstanceGroupType":"MASTER","InstanceType":"m4.4xlarge","Name":"Driver","BidPrice":"0.20"},{"InstanceCount":4,"InstanceGroupType":"CORE","InstanceType":"m4.4xlarge","Name":"Worker nodes","BidPrice":"0.20"}]' \
+  --instance-groups '
+[
+  {
+    "InstanceCount": 1,
+    "EbsConfiguration": {
+      "EbsBlockDeviceConfigs": [
+        {
+          "VolumeSpecification": {
+            "SizeInGB": 128,
+            "VolumeType": "gp2"
+          },
+          "VolumesPerInstance": 1
+        }
+      ]
+    },
+    "InstanceGroupType": "MASTER",
+    "InstanceType": "m4.4xlarge",
+    "Name": "Driver",
+    "BidPrice": "0.20"
+  },
+  {
+    "InstanceCount": 4,
+    "EbsConfiguration": {
+      "EbsBlockDeviceConfigs": [
+        {
+          "VolumeSpecification": {
+            "SizeInGB": 128,
+            "VolumeType": "gp2"
+          },
+          "VolumesPerInstance": 1
+        }
+      ]
+    },
+    "InstanceGroupType": "CORE",
+    "InstanceType": "m4.4xlarge",
+    "Name": "Worker nodes",
+    "BidPrice": "0.20"
+  }
+]' \
   --log-uri 's3://logs.multe.co.uk/elasticmapreduce/' \
   --enable-debugging \
   --use-default-roles \
